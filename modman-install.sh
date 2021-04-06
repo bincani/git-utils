9b07b347f5e04353bc0af4483bb1710fc2c784e1
ben.incani <ben.incani@factoryx.com.au>
2021-04-06 14:37:07 +1000

    modman install script

---
+++ modman-install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# USAGE:
+# alias mi='/home/factoryx/git-utils/modman-install.sh'
+# mi FactoryX_CmApi
+
+# get param to lowercase
+MODULE=$1
+MODULE=$(echo "$MODULE" | sed 's/\(.*\)/\L\1/')
+
+echo "installing $MODULE"
+
+# test url 401 Unauthorized is OK
+URL=https://bincani@bitbucket.org/factoryx-developers/$MODULE.git
+
+RCODE=$(wget --server-response $URL 2>&1 | awk '/^  HTTP/{print $2}')
+#echo "$RCODE"
+
+# if wget $URL >/dev/null 2>&1 ; then
+if [[ "$RCODE" == 404 ]]; then
+    echo "cannot find repo $URL $RCODE"
+    exit 1
+else
+    REPO=git@bitbucket.org:factoryx-developers/$MODULE.git    
+fi
+
+if [ -z "$(git status --porcelain)" ]; then 
+    modman clone $REPO
+    modman update $MODULE --force --copy
+    git add .
+    git commit -m "update $MODULE"
+    git push  
+else 
+    echo "Cannot install $MODULE as there are Uncommitted changes!"
+    git status --porcelain
+fi
+
