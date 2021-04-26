80105c7ed1678eb0cdf47f60529cab59c4afe98d
ben.incani <ben.incani@factoryx.com.au>
2021-04-26 14:45:15 +1000

    english

--- modman-install.sh
+++ modman-install.sh
@@ -27,7 +27,7 @@ fi
 if [ -z "$(git status --porcelain)" ]; then 
     CBRANCH=$(git -C .modman/$MODULE symbolic-ref --short -q HEAD)
     if [[ "$CBRANCH" != 'master' ]]; then
-        echo "$MODULE is on the master branch (current: $CBRANCH)"
+        echo "$MODULE is NOT on the master branch (current: $CBRANCH)"
         exit 1
     fi
     git pull
